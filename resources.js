9c27597f1e581340e0c1023b2c46d7f2215ffa78
WriteOftenPlayNever <mostlywriterrarelyplayer@gmail.com>
2021-10-26 02:49:26 +1100

    bug fix

--- resources.js
+++ resources.js
@@ -5,8 +5,8 @@ export const grandom = (min, max) => ((Math.random() + Math.random()) / 2) * (ma
 export const randInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
 export const setImageAlpha = (img, alpha) => {
     img.loadPixels();
-    for (y = 0; y < img.height; y++) {
-        for (x = 0; x < img.width; x++) {
+    for (let y = 0; y < img.height; y++) {
+        for (let x = 0; x < img.width; x++) {
             let index = (x + y * width) * 4;
             img.pixels[index] = (alpha * 255);
         }
