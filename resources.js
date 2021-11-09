aa507ee1d11dd479c0b8af9d580d5236b9e7c451
WriteOftenPlayNever <mostlywriterrarelyplayer@gmail.com>
2021-11-10 00:14:53 +1100

    bug fix

--- resources.js
+++ resources.js
@@ -3,6 +3,7 @@
 export const objCopy = object => JSON.parse(JSON.stringify(object));
 export const grandom = (min, max) => ((Math.random() + Math.random()) / 2) * (max - min) + min;
 export const randInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
+export const randSelect = list => list[randInt(0, list.length)];
 export const setImageAlpha = (img, alpha) => {
     img.loadPixels();
     for (let index = 0; index < img.pixels.length; index++) {
